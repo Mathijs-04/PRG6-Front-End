5da8bcb84d8b877007e74bccc83786f753015679
Mathijs-04 <mathijsvandermeijde@gmail.com>
2025-01-31 11:01:08 +0100

    Updated 404 page

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -3,7 +3,7 @@ function NotFound() {
         <div className="flex flex-col items-center justify-center">
             <h1 className="text-6xl font-bold mb-2">404</h1>
             <h2 className="text-4xl mb-2">There is no page here</h2>
-            <img className="mt-4 size-1/8" src="https://media.tenor.com/nYdAr6rffx0AAAAM/an-emoji-disintegrating-in-pain.gif" alt="404"/>
+            <img className="mt-4 size-1/12" src="https://png.pngtree.com/png-clipart/20240512/original/pngtree-burning-laptop-on-desk-office-fire-hazard-png-image_15075167.png" alt="404"/>
         </div>
     )
 }
